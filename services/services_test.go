ab7b15774077c08401d13bea08f0151ed753c154
SinaKhorsandi <sina76khorsandi@gmail.com>
2021-10-04 14:22:13 +0330

    create test for services

---
+++ services/services_test.go
@@ -0,0 +1,44 @@
+package wallet
+
+import (
+	"github.com/sinakhorsandi/e-wallet/db"
+	"github.com/sinakhorsandi/testify/assert"
+	"testing"
+)
+
+func TestCheckCustomerExist(t *testing.T) {
+	a:=assert.New(t)
+	s:=Service{}
+	s.userRegister("sina","sinakhorsandi@gmail.com")
+	b:=s.checkCustomerExist("sinakhorsandi@gmail.com")
+	a.Equal(true,b)
+}
+func TestUserRegistration(t *testing.T) {
+	a:=assert.New(t)
+	s:=Service{}
+	s.userRegister("sina","sinakhorsandi@gmail.com")
+	a.Equal(1,len(s.personalAccounts))
+}
+func TestCheckPersonalAccountById(t *testing.T) {
+	a:=assert.New(t)
+	s:=Service{}
+	s.userRegister("sina","sinakhorsandi@gmail.com")
+	account,_:=s.checkPersonalAccountById(1)
+	a.Equal("sina",account.UserName)
+}
+func TestCheckPersonalWallet(t *testing.T) {
+	a:=assert.New(t)
+	s:=Service{}
+	s.userRegister("sina","sinakhorsandi@gmail.com")
+	account,_:=s.checkPersonalWallet(1)
+	a.Equal(1,account.WalletId)
+}
+func TestDeposit(t *testing.T) {
+	a:=assert.New(t)
+	s:=Service{}
+	s.userRegister("sina","sinakhorsandi@gmail.com")
+	wal,_:=s.deposit(1,db.Money(1000000))
+	a.Equal(db.Money(1000000),wal.Balance)
+}
+
+
