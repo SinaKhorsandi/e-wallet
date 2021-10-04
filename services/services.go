2696a7ec98a31aa3fd1d245974371666d0f900e3
SinaKhorsandi <sina76khorsandi@gmail.com>
2021-10-04 13:46:07 +0330

    fixed userRegisteration function

--- services/services.go
+++ services/services.go
@@ -12,14 +12,14 @@ const (
 	UserIsExist                 Status = "user is exist"
 	OrganIsExist                Status = "organization is exist"
 	NegativeAmount              Status = "Amount must be positive"
-	AccountNotExist				Status = "Account does not exist"
+	AccountNotExist             Status = "Account does not exist"
 	TransactionStatusOk         Status = "ok"
 	TransactionStatusFail       Status = "fail"
 	TransactionStatusInProgress Status = " in progress"
 )
 
 type Service struct {
-	nextAccountId        int
+	nextAccountId int
 
 	//Have nextAccountId and nextOrganizationId both???
 	//nextOrganizationId        int
@@ -31,7 +31,7 @@ type Service struct {
 	mock.Mock
 }
 
-func (s *Service) userRegister(userName,email string) error {
+func (s *Service) userRegister(userName, email string) error {
 	b := s.checkCustomerExist(email)
 	if b == true {
 		return fmt.Errorf("%s", UserIsExist)
@@ -39,58 +39,64 @@ func (s *Service) userRegister(userName,email string) error {
 	s.nextAccountId++
 	s.nextWalletId++
 	account := &db.PersonalAccount{
-		UserId:s.nextAccountId   ,
+		UserId:   s.nextAccountId,
 		UserName: userName,
 		Email:    email,
 	}
 	// create new function for adding wallet!!!????
 	wallet := &db.Wallet{
 		WalletId: s.nextWalletId,
-		UserId:  account.UserId,
-		Balance: 0,
+		UserId:   account.UserId,
+		Balance:  0,
 	}
 	s.personalAccounts = append(s.personalAccounts, account)
 	s.wallets = append(s.wallets, wallet)
 	return nil
 }
 
-func (s *Service) organizationRegister()  {
+func (s *Service) organizationRegister() {
 
 }
 
 func (s *Service) checkCustomerExist(email string) bool {
-	ret := s.Called(email)
-	err := ret.Error(0)
-	if err != nil {
-		return false
+	for _, account := range s.personalAccounts {
+		if account.Email == email {
+			return true
+		}
 	}
-	return true
+	return false
+	//ret := s.Called(email)
+	//err := ret.Error(0)
+	//if err != nil {
+	//	return false
+	//}
+	//return true
 }
 
-func (s *Service) deposit(accountId int,amount db.Money) error  {
+func (s *Service) deposit(accountId int, amount db.Money) error {
 	var wallet *db.Wallet
-	if amount<=0 {
+	if amount <= 0 {
 		return fmt.Errorf("%s\n", NegativeAmount)
 	}
-	_,err:=s.checkPersonalAccountById(accountId)
+	_, err := s.checkPersonalAccountById(accountId)
 	if err != nil {
-		fmt.Errorf("%s\n",err)
+		fmt.Errorf("%s\n", err)
 	}
 
 	//Check
-	if wallet.UserId==accountId {
-		wallet.Balance+=amount
+	if wallet.UserId == accountId {
+		wallet.Balance += amount
 		return nil
 	}
 	return nil
-	}
-func (s *Service) checkPersonalAccountById(accountId int) (*db.PersonalAccount,error)  {
-	for _,acc:=range s.personalAccounts{
-		if acc.UserId == accountId{
-			return acc,nil
+}
+func (s *Service) checkPersonalAccountById(accountId int) (*db.PersonalAccount, error) {
+	for _, acc := range s.personalAccounts {
+		if acc.UserId == accountId {
+			return acc, nil
 		}
 	}
-	return nil,fmt.Errorf("%s\n",AccountNotExist)
+	return nil, fmt.Errorf("%s\n", AccountNotExist)
 }
 
 //func (s *Service) checkOrganizationAccountById(accountId int) bool  {
