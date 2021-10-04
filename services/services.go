3e551689e2b5c3258876e741b5b9f84d64aad001
SinaKhorsandi <sina76khorsandi@gmail.com>
2021-10-04 10:35:32 +0330

    create registration function for users and organizations

--- services/services.go
+++ services/services.go
@@ -1,46 +1,78 @@
 package wallet
 
 import (
+	"fmt"
+	"github.com/sinakhorsandi/e-wallet/db"
 	"github.com/sinakhorsandi/testify/mock"
 )
 
 type Status string
+
 const (
-	UserIsExist 				Status = "user is exist"
-	NegativeAmount				Status = "Amount must be positive"
+	UserIsExist                 Status = "user is exist"
+	OrganIsExist                Status = "organization is exist"
+	NegativeAmount              Status = "Amount must be positive"
 	TransactionStatusOk         Status = "ok"
 	TransactionStatusFail       Status = "fail"
 	TransactionStatusInProgress Status = " in progress"
 )
 
 type Service struct {
-	//accounts []*db.PersonalAccount
-	//wallets      []*db.Wallet
-	//payments      []*db.Transaction
+	nextUserId        int
+	nextOrganizationId        int
+	nextWalletId         int
+	personalAccounts     []*db.PersonalAccount
+	organizationAccounts []*db.OrganizationAccount
+	wallets              []*db.Wallet
+	payments             []*db.Transaction
 	mock.Mock
 }
 
-
-//type Call struct {
-//
-//}
-
-//func (s *Service) userRegister(user *db.PersonalAccount) error {
-//	b:=s.checkCustomerExist(user.Email)
-//	if b==true {
-//		return fmt.Errorf("%s",UserIsExist)
-//	}
-//	account :=&db.PersonalAccount{
-//		UserId:user.UserId ,
-//		UserName: user.UserName,
-//		Email: user.Email,
-//	}
-//
-//}
-
-func organizationRegister()  {
+func (s *Service) userRegister(user *db.PersonalAccount) error {
+	b := s.checkCustomerExist(user.Email)
+	if b == true {
+		return fmt.Errorf("%s", UserIsExist)
+	}
+	s.nextUserId++
+	s.nextWalletId++
+	account := &db.PersonalAccount{
+		UserId:s.nextUserId   ,
+		UserName: user.UserName,
+		Email:    user.Email,
+	}
+	//check for creating new function for adding wallet!!!????
+	wallet := &db.Wallet{
+		WalletId: s.nextWalletId,
+		UserId:   user.UserId,
+		Balance: 0,
+	}
+	s.personalAccounts = append(s.personalAccounts, account)
+	s.wallets = append(s.wallets, wallet)
+	return nil
 }
 
+func (s *Service) organizationRegister(organization *db.OrganizationAccount) error {
+	b := s.checkCustomerExist(organization.Email)
+	if b == true {
+		return fmt.Errorf("%s", OrganIsExist)
+	}
+	s.nextOrganizationId++
+	s.nextWalletId++
+	account := &db.OrganizationAccount{
+		OrganizationId:   s.nextOrganizationId,
+		OrganizationName: organization.OrganizationName,
+		Email:            organization.Email,
+	}
+	//check for creating new function for adding wallet!!!????
+	wallet := &db.Wallet{
+		WalletId:s.nextWalletId ,
+		UserId:   organization.OrganizationId,
+		Balance: 0,
+	}
+	s.organizationAccounts = append(s.organizationAccounts, account)
+	s.wallets = append(s.wallets, wallet)
+	return nil
+}
 
 func (s *Service) checkCustomerExist(email string) bool {
 	ret := s.Called(email)
@@ -51,29 +83,8 @@ func (s *Service) checkCustomerExist(email string) bool {
 	return true
 }
 
-//func (s *Service) deposit(WalletId int, amount db.Money) error {
-//	if amount <= 0 {
-//		return fmt.Errorf("%s",NegativeAmount)
-//	}
-//
-//	var wallet *db.Wallet
-//	for _, wal := range s.wallets {
-//		if wal.WalletId == WalletId {
-//			wallet = wal
-//			break
-//		}
-//	}
-//
-//	if wallet == nil {
-//		return fmt.Errorf("error")
-//	}
-//
-//	wallet.Balance += amount
-//	return nil
-//}
-//
-//func pay()  {
-//
-//}
+func deposit()  {
+}
 
-
+func pay() {
+}
